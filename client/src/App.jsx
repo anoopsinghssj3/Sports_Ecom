0688b2c743f6305a3651072ac0946cfac2d4d784
Anoop Singh <anoopsinghssj3@gmail.com>
2025-07-04 08:33:53 +0530

    godhand

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // includes Popper
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1> Sports</h1>
+    </>
+  )
+}
+
+export default App
