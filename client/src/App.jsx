fd94bb1ea1f3f45ac214187fa0104922134dde5d
Anoop Singh <anoopsinghssj3@gmail.com>
2025-07-06 06:04:51 +0530

    sf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // includes Popper
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { useState } from 'react'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/home/Home';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
 
   return (
     <>
-      <h1> Sports</h1>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
